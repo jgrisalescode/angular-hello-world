5ca690b065838a1b481004da617bac6f8673c6a8
JuliaGrisales <jgrisales@gmail.com>
2020-07-04 12:29:13 +0200

    Using *ngIf and *ngFor

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-body',
   templateUrl: './body.component.html',
 })
-export class BodyComponent {}
+export class BodyComponent {
+  mostrar: boolean = true;
+  frase: any = {
+    mensaje: 'Un gran poder requiere una gran responsabilidad',
+    autor: 'Ben Parker',
+  };
+  personajes: string[] = ['Spiderman', 'Venom', 'Octopus'];
+}
